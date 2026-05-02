4dda1b587a626578b838882e9392330bf7a83cea
KwerkQ <filasimonov4@gmail.com>
2026-05-02 11:11:28 +0300

    add widen powerup

--- main.go
+++ main.go
@@ -1,16 +1,15 @@
 package main
 
 import (
-	//"fmt"
-
 	"strconv"
 
 	rl "github.com/gen2brain/raylib-go/raylib"
 )
 
 type Player struct {
-	rec   rl.Rectangle
-	color rl.Color
+	rec          rl.Rectangle
+	color        rl.Color
+	widenpowerup int
 }
 type Ball struct {
 	rec      rl.Rectangle
@@ -31,27 +30,31 @@ func DrawGame(game *Game) {
 	rl.DrawRectangleRec(game.ball.rec, game.ball.color)
 	rl.DrawRectangleRec(game.player1.rec, game.player1.color)
 	rl.DrawRectangleRec(game.player2.rec, game.player2.color)
+	rl.DrawText(strconv.Itoa(game.player1.widenpowerup), 100, 75, 35, rl.Black)
+	rl.DrawText(strconv.Itoa(game.player2.widenpowerup), 680, 75, 35, rl.Black)
 }
 func UpdateGame(game *Game) {
+	speedpaddle := 6
+
 	var dir int32
-	if game.player1.rec.Y != 450 {
+	if game.player1.rec.Y <= 600-game.player1.rec.Height {
 		if rl.IsKeyDown(rl.KeyS) {
-			game.player1.rec.Y += 5
+			game.player1.rec.Y += float32(speedpaddle)
 		}
 	}
-	if game.player1.rec.Y != 0 {
+	if game.player1.rec.Y >= 0 {
 		if rl.IsKeyDown(rl.KeyW) {
-			game.player1.rec.Y -= 5
+			game.player1.rec.Y -= float32(speedpaddle)
 		}
 	}
-	if game.player2.rec.Y != 450 {
+	if game.player2.rec.Y <= 600-game.player2.rec.Height {
 		if rl.IsKeyDown(rl.KeyDown) {
-			game.player2.rec.Y += 5
+			game.player2.rec.Y += float32(speedpaddle)
 		}
 	}
-	if game.player2.rec.Y != 0 {
+	if game.player2.rec.Y >= 0 {
 		if rl.IsKeyDown(rl.KeyUp) {
-			game.player2.rec.Y -= 5
+			game.player2.rec.Y -= float32(speedpaddle)
 		}
 	}
 	if rl.IsKeyDown(rl.KeySpace) && game.started != true {
@@ -64,22 +67,33 @@ func UpdateGame(game *Game) {
 		game.ball.velocity.Y = float32(rl.GetRandomValue(-4, 4))
 		game.started = true
 	}
+	if rl.IsKeyDown(rl.KeyLeftShift) && game.player1.widenpowerup > 0 && game.player1.rec.Height != 200 {
+		game.player1.widenpowerup -= 1
+		game.player1.rec.Y -= 25
+		game.player1.rec.Height = 200
+	}
+	if rl.IsKeyDown(rl.KeyRightShift) && game.player2.widenpowerup > 0 && game.player2.rec.Height != 200 {
+		game.player2.widenpowerup -= 1
+		game.player2.rec.Y -= 25
+		game.player2.rec.Height = 200
+	}
+
 	if rl.CheckCollisionRecs(game.player1.rec, game.ball.rec) {
 		game.ball.velocity.X *= -1
 		if rl.IsKeyDown(rl.KeyW) {
-			game.ball.velocity.Y += float32(rl.GetRandomValue(3, 5))
+			game.ball.velocity.Y += 4 //float32(rl.GetRandomValue(3, 5))
 		}
 		if rl.IsKeyDown(rl.KeyS) {
-			game.ball.velocity.Y += float32(rl.GetRandomValue(-3, -5))
+			game.ball.velocity.Y += -4 //float32(rl.GetRandomValue(-3, -5))
 		}
 	}
 	if rl.CheckCollisionRecs(game.player2.rec, game.ball.rec) {
 		game.ball.velocity.X *= -1
 		if rl.IsKeyDown(rl.KeyUp) {
-			game.ball.velocity.Y += float32(rl.GetRandomValue(3, 5))
+			game.ball.velocity.Y += 4 //float32(rl.GetRandomValue(3, 5))
 		}
 		if rl.IsKeyDown(rl.KeyDown) {
-			game.ball.velocity.Y += float32(rl.GetRandomValue(-3, -5))
+			game.ball.velocity.Y += -4 //float32(rl.GetRandomValue(-3, -5))
 		}
 	}
 	if game.ball.rec.Y <= 0 {
@@ -103,6 +117,15 @@ func UpdateGame(game *Game) {
 		game.ball.velocity.Y = 0
 		game.ball.rec.X = 390
 		game.ball.rec.Y = 290
+		if game.player1.rec.Height == 200 {
+			game.player1.rec.Height = 150
+			game.player1.rec.Y += 25
+		}
+		if game.player2.rec.Height == 200 {
+			game.player2.rec.Height = 150
+			game.player2.rec.Y += 25
+		}
+
 	}
 	if game.ball.rec.X >= 800 {
 		game.score1 += 1
@@ -111,6 +134,15 @@ func UpdateGame(game *Game) {
 		game.ball.velocity.Y = 0
 		game.ball.rec.X = 390
 		game.ball.rec.Y = 290
+		if game.player1.rec.Height == 200 {
+			game.player1.rec.Height = 150
+			game.player1.rec.Y += 25
+		}
+		if game.player2.rec.Height == 200 {
+			game.player2.rec.Height = 150
+			game.player2.rec.Y += 25
+		}
+
 	}
 	game.ball.rec.X += game.ball.velocity.X
 	game.ball.rec.Y += game.ball.velocity.Y
@@ -123,17 +155,22 @@ func UpdateGame(game *Game) {
 }
 
 func main() {
+
+	//TODO add a point counter for each side and after a person returns the ball, they gain points. After that person gains a certain ammount of points, they get to activate a powerup which makes the ball they hit go super fast
+
 	velocity := rl.Vector2{
 		X: 0,
 		Y: 0,
 	}
 	player1 := Player{
-		rec:   rl.NewRectangle(10, 225, 10, 150),
-		color: rl.Beige,
+		rec:          rl.NewRectangle(10, 225, 10, 150),
+		color:        rl.Beige,
+		widenpowerup: 3,
 	}
 	player2 := Player{
-		rec:   rl.NewRectangle(780, 225, 10, 150),
-		color: rl.Beige,
+		rec:          rl.NewRectangle(780, 225, 10, 150),
+		color:        rl.Beige,
+		widenpowerup: 3,
 	}
 	ball := Ball{
 		rec:      rl.NewRectangle(390, 290, 20, 20),
@@ -152,7 +189,7 @@ func main() {
 	rl.InitWindow(800, 600, "ping pong pang")
 	defer rl.CloseWindow()
 
-	rl.SetTargetFPS(75)
+	rl.SetTargetFPS(60)
 
 	for !rl.WindowShouldClose() {
 		rl.BeginDrawing()
